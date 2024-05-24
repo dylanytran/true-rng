ab2183168bbaf417f9f534609ea02d08cb413be6
Jadel15 <jadafali15@gmail.com>
2024-05-24 13:54:47 +0900

    bon

---
+++ myproject/security-frontend-master/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+
+function App() {
+  useEffect(() => {
+    document.title = "Dylan Tran";
+  }, []);
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
