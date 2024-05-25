9a7151e11486f5ad6114cf958f70d304b71cc5c1
Dylan Tran <dylanytran@gmail.com>
2024-05-26 02:11:09 +0900

    fixed bug improved ui

--- myproject/security-frontend-master/src/pages/Home.js
+++ myproject/security-frontend-master/src/pages/Home.js
@@ -1,9 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "../styles/Home.css";
-
-// myenv/bin/activate
-// python manage.py runserver
 
 function Home() {
   const [randomNumber, setRandomNumber] = useState(null);
@@ -12,15 +9,23 @@ function Home() {
   const [loading, setLoading] = useState(false);
 
   const handleGenerateRandom = () => {
-    if (maxValue <= minValue) {
+    const min = parseInt(minValue, 10);
+    const max = parseInt(maxValue, 10);
+
+    if (max <= min) {
       alert("Maximum value must be greater than minimum value!");
+      return;
+    }
+
+    if (isNaN(max) || isNaN(min)) {
+      alert("Please fill out a minimum and minimum value!");
       return;
     }
 
     setLoading(true);
 
     axios
-      .get(`http://127.0.0.1:8000/api/random/${minValue}/${maxValue}/`)
+      .get(`http://127.0.0.1:8000/api/random/${min}/${max}/`)
       .then((response) => {
         setRandomNumber(response.data["random number"]);
         setLoading(false);
@@ -32,11 +37,11 @@ function Home() {
   };
 
   const handleMinChange = (event) => {
-    setMinValue(parseInt(event.target.value));
+    setMinValue(event.target.value);
   };
 
   const handleMaxChange = (event) => {
-    setMaxValue(parseInt(event.target.value));
+    setMaxValue(event.target.value);
   };
 
   return (
@@ -45,12 +50,20 @@ function Home() {
       <div className="inputs">
         <div className="box">
           <label>Min: </label>
-          <input type="number" value={minValue} onChange={handleMinChange} />
+          <input
+            type="number"
+            value={minValue}
+            onChange={handleMinChange}
+          />
         </div>
 
         <div className="box">
           <label>Max: </label>
-          <input type="number" value={maxValue} onChange={handleMaxChange} />
+          <input
+            type="number"
+            value={maxValue}
+            onChange={handleMaxChange}
+          />
         </div>
 
         <button onClick={handleGenerateRandom}>Generate!</button>
