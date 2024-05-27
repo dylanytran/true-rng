aaf3491097b3d57cfd7ee3b79992fda77ebeabe5
Jadel15 <jadafali15@gmail.com>
2024-05-27 21:05:10 +0900

    reverted backend.py and created presentation code

--- presentation code.py
+++ presentation code.py
@@ -4,7 +4,6 @@ import hashlib
 import requests
 
 model_name = "openai-community/gpt2"
-
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
@@ -12,24 +11,23 @@ def gen_text(prompt):
 
     input_text = prompt
     input_ids = tokenizer.encode(input_text, return_tensors="pt")
-
     attention_mask = torch.ones(input_ids.shape, dtype=torch.long)
     
-    # Generate text with adjusted parameters
+    # generate text with adjusted parameters
     outputs = model.generate(
         input_ids,
         max_length=100,
         num_return_sequences=1,
-        temperature=0.7,
-        top_k=50,
-        top_p=0.95,
-        repetition_penalty=1.2,
-        do_sample=True,
-        pad_token_id=tokenizer.eos_token_id,
-        attention_mask=attention_mask
+        temperature=0.7, 
+        top_k=50, 
+        top_p=0.95,  
+        repetition_penalty=1.2,  
+        do_sample=True, 
+        pad_token_id=tokenizer.eos_token_id,  
+        attention_mask=attention_mask  
     )
 
-    # Decode the generated text
+    # decode the generated text
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
 
@@ -55,13 +53,30 @@ def gen_atmosphere():
 def rand_gen(min, max):
     # AI
     generated_text = gen_text("This is a")
+    print("\nRandom sentence: ", generated_text)
+
     text_hash = gen_hash(generated_text)
+    print("SHA-256 hash: ", text_hash)
+
     small_hash = text_hash % 1000000000
+    print("Shortened hash: ", small_hash)
+
     
-    #Atmosphere
+    # atmospheric noise
     atmospheric_value = gen_atmosphere()
+    print("Atmospheric noise: ", atmospheric_value)
+
     xor = small_hash ^ atmospheric_value
+    print("XOR value: ", xor)
     
     rang = max - min + 1
     final_val = (xor % rang) + min
     return final_val
+
+print("Please enter a minimum value.")
+min = int(input())
+
+print("Please enter a maxmimum value.")
+max = int(input())
+
+print("Your random number is : ", rand_gen(min, max), "\n")
